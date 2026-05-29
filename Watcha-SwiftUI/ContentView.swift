34edb64137a859e6d6fef1ba95838cd01f888566
seunghee <daalgwabyul@naver.com>
2026-05-29 13:26:33 +0900

    initial commit

---
+++ Watcha-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Watcha-SwiftUI
+//
+//  Created by 초긍정행운의포춘쿠키 on 5/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Watcha-SwiftUI/Watcha_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watcha_SwiftUIApp.swift
+//  Watcha-SwiftUI
+//
+//  Created by 초긍정행운의포춘쿠키 on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct Watcha_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
